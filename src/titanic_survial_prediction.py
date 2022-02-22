074c8ae36117f941f37f02720ad897888d63fa86
Justin Hu <geek.huqijun@gmail.com>
2022-02-21 22:09:10 -0500

    Kaggle Machine Learning Competition: Titanic Survival Predication:  Data Exploration

--- src/titanic_survial_prediction.py
+++ src/titanic_survial_prediction.py
@@ -5,7 +5,9 @@ by Justin Hu
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
+import seaborn as sb
 
 class TitanicSurvival:
     """
@@ -36,15 +38,20 @@ class TitanicSurvival:
     """
 
     def __init__(self):
-        self.df = None
+        self.df_train = None
+        self.df_test = None
 
     def load_data(self):
         """
         load data from csv
         :return:
         """
-        self.df = pd.read_csv('train.csv')
-        print(f'data loaed. data shape: {self.df.shape}')
+        self.df_train = pd.read_csv('train.csv')
+        self.df_train.name = 'Training Set'
+        print(f'train data loaded. data shape: {self.df_train.shape}')
+        self.df_test = pd.read_csv('test.csv')
+        self.df_test.name = 'Test Set'
+        print(f'test data loaded. data shape: {self.df_test.shape}')
 
     def explore_data(self):
         """
@@ -54,28 +61,8 @@ class TitanicSurvival:
         # General info about the dataset
         # the train data is a 891 x 12 matrix.
         # of 891 rows,  342 survived.
-        print(f'data set info: {self.df.info}')
-        print(f'columns: {self.df.columns}')
-        print(f"total rows: {self.df.shape[0] }, number of survived: "
-              f"{ self.df.loc[self.df['Survived'] == 1].shape[0]}, "
-              f"number of unsurvived: {self.df.loc[self.df['Survived'] == 0].shape[0]}")
-
-        # check null value in data, seem like we have some missing value for Cabin number
-        # 687 rows have missing cabin number.
-        # Since the majority rows have missed  cabin number,  I will not use it as a feature
-        null_check = self.df.isnull()
-        print(null_check)
-        print(f"{ null_check.loc[null_check['Cabin'] == True].shape[0]} rows have missing cabin number")
-
-
-        # pclass types
-        #Pclass values: [1 2 3]
-        print(f" Pclass values: {np.unique(self.df['Pclass'])}")
-
-        # Sex values
-        # ['female' 'male']
-        print(f" Sex values: {np.unique(self.df['Sex'])}")
-
+        print(f'data set info:')
+        print(self.df_train.info())
 
         # Show Summary Stats for Age, SibSp, Parch, Fare
         #            Age       SibSp       Parch        Fare
@@ -89,12 +76,81 @@ class TitanicSurvival:
         # max     80.000000    8.000000    6.000000  512.329200
 
         # Wow,  there are also some missing for age column
-        print(self.df[['Age', 'SibSp', 'Parch', 'Fare']].describe())
+        print(self.df_train.describe())
 
-        #explore correlations with different columns
+        number_survived = self.df_train.loc[self.df_train['Survived'] == 1].shape[0]
+        number_unsurvived = self.df_train.loc[self.df_train['Survived'] == 0].shape[0]
+
+        print(f"total rows: {self.df_train.shape[0] }, number of survived: "
+              f"{ number_survived }, "
+              f"number of unsurvived: { number_unsurvived }, "
+              f"survival ratio { number_survived / (number_survived + number_unsurvived)}")
+
+        # pclass types
+        #Pclass values: [1 2 3]
+        print(f" Pclass values: {np.unique(self.df_train['Pclass'])}")
+
+        # Sex values
+        # ['female' 'male']
+        print(f" Sex values: {np.unique(self.df_train['Sex'])}")
+
+        #Missing values
+        #              Total     %
+        # Cabin          687  77.1
+        # Age            177  19.9
+        # Embarked         2   0.2
+        missing = self.df_train.isnull().sum().sort_values(ascending=False)
+        percent = self.df_train.isnull().sum() / self.df_train.isnull().count() * 100
+        percent = (round(percent, 1)).sort_values(ascending=False)
+        missing = pd.concat([missing, percent], axis=1, keys=['Total', '%'])
+        print(missing)
 
 
-    def data_preprocessing(self):
+        # exlore data further with visualization
+        # survived vs unsurvied
+        f, ax = plt.subplots()
+        ax.set_title('Survived')
+
+        sb.countplot('Survived', data=self.df_train, ax=ax)
+        plt.show()
+
+        # Pclass, Age vs Survived
+        f, ax = plt.subplots()
+        sb.violinplot('Pclass', 'Age', data=self.df_train, split=True, hue='Survived', ax=ax)
+        ax.set_title('Plass and Age vs Survived')
+        ax.set_yticks(range(0, 110, 10))
+        plt.show()
+
+        # Pclass
+        sb.barplot(x='Pclass', y='Survived', hue='Sex', data=self.df_train)
+        plt.show()
+
+
+        # Sex and Age vs Survivied
+        f, ax = plt.subplots()
+        sb.violinplot('Sex', 'Age', data=self.df_train, split=True, hue='Survived', ax=ax)
+        ax.set_title('Sex and Age vs Survived')
+        ax.set_yticks(range(0, 110, 10))
+        plt.show()
+
+        # Embarked vs Survived
+        sb.factorplot('Embarked', 'Survived', data=self.df_train)
+        plt.show()
+
+        # SibSp and Parch, combined into family size
+        data1 = self.df_train.copy()
+        data1['Family_size'] = data1['SibSp'] + data1['Parch'] + 1
+        print(data1['Family_size'].value_counts().sort_values(ascending=True))
+        sb.factorplot('Family_size', 'Survived', data=data1, aspect=2.5)
+        plt.show()
+
+
+    def data_preprocessing(self, df):
+        """
+        data preprocessing
+        :param df:
+        :return:
+        """
         pass
 
 
@@ -102,7 +158,6 @@ def main():
     c = TitanicSurvival()
     c.load_data()
     c.explore_data()
-
 
 
 if __name__ == '__main__':
