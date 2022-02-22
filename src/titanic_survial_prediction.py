d38d5c1665f1ac55a1ea7d3056a5f15819f71a6d
Justin Hu <geek.huqijun@gmail.com>
2022-02-22 17:41:50 -0500

    Kaggle Machine Learning Competition: Titanic Survival Predication 3. prediction 4. save to submission to Kaggle

--- src/titanic_survial_prediction.py
+++ src/titanic_survial_prediction.py
@@ -2,14 +2,20 @@
 Titanic Survival Prediction
 by Justin Hu
 2022/02/19
+Tested on Python version:  3.9
 """
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sb
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.model_selection import cross_validate, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from xgboost import XGBClassifier
 
-# For debugging purpose, allowing more columns be printed out in the console
+# For debugging purpose, allowing more columns be printed out to the console
 desired_width = 320
 pd.set_option('display.width', desired_width)
 np.set_printoptions(linewidth=desired_width)
@@ -26,10 +32,11 @@ class TitanicSurvival:
         4. model training and selection(performance comparison)
         5. make predications and output it to kaggle submission format
 
-    Algorithms compared here:
+    Algorithms used and compared:
         1. logistic regression
-        2. Random forest
-        3. XGBoost
+        2. Linear -SVM
+        3. Random forest
+        4. XGBoost
 
     Data set columns:
         survival	Survival	0 = No, 1 = Yes
@@ -48,6 +55,7 @@ class TitanicSurvival:
         self.df_train = None
         self.df_test = None
         self.df_all = None
+        self.model = None
 
     def load_data(self):
         """
@@ -75,7 +83,7 @@ class TitanicSurvival:
         # 3. Pclass,  Age,  Sex do impact the probability of survival
         # 4. Family_size(SibSP + Parch) does impact the chance of survival
         # 5. Embarked may impact the probability of survival
-        # 6. Cabin has a missing rate of 77.1%,  can't use it directly
+        # 6. Cabin has a missing rate of 77.1%,  can't use it directly. Will not use it initially.
         # 7. Age has a missing rate of 19.9, something needs to be done.
         # 8. Fare may impact the survival probability
 
@@ -239,7 +247,7 @@ class TitanicSurvival:
         #  10  Survived     891 non-null    float64
         #  11  Ticket       1309 non-null   object
         #  As shown above, all feature except for Cabin have 1209 non-null values.
-        #  Right now i just leave out Cabin, dealing with it later maybe
+        #  Right now I just leave out Cabin, dealing with it later maybe if necessary
         print()
         print('_' * 25)
         print('All set info after filling missing values:')
@@ -261,7 +269,7 @@ class TitanicSurvival:
         # Grouping ticket by their frequency
         self.df_all['Ticket_Frequency'] = self.df_all.groupby('Ticket')['Ticket'].transform('count')
 
-        # Interpreting Name to Title and IsMarried
+        # Interpreting and transforming Name to Title and IsMarried
         self.df_all['Title'] = self.df_all['Name'].str.split(', ', expand=True)[1].str.split('.', expand=True)[0]
 
         self.df_all['Is_Married'] = 0
@@ -321,12 +329,174 @@ class TitanicSurvival:
         print(self.df_test.info())
         print(self.df_test.head(10))
 
+    def train_and_evaluate_model(self):
+        """
+        train and evaluate the results of 4 different models with cross validation:
+            1. logistic regression
+            2. Linear -SVM
+            3. Random forest
+            4. XGBoost
+
+        here, we use 10 (k = 10) folds in cross validation
+        :return:
+        """
+        y_train = self.df_train['Survived'].values
+        X_train = self.df_train.drop(columns=['Survived'])
+        X_train = StandardScaler().fit_transform(X_train)
+
+        # Train models and calculate their scores
+        logreg = LogisticRegression()
+        logreg.fit(X_train, y_train)
+        acc_log = round(logreg.score(X_train, y_train) * 100, 2)
+
+        linear_svc = LinearSVC()
+        linear_svc.fit(X_train, y_train)
+        acc_linear_svc = round(linear_svc.score(X_train, y_train) * 100, 2)
+
+        random_forest = RandomForestClassifier(n_estimators=100)
+        random_forest.fit(X_train, y_train)
+        acc_random_forest = round(random_forest.score(X_train, y_train) * 100, 2)
+
+        xgb = XGBClassifier(n_estimators=100)
+        xgb.fit(X_train, y_train)
+        acc_xgb = round(xgb.score(X_train, y_train) * 100, 2)
+
+        model_name = ['Logistic Regression', 'Linear SVM', 'Random Forest', 'XGBoosting']
+        results = pd.DataFrame({
+            'Model': model_name,
+            'Score': [acc_log, acc_linear_svc, acc_random_forest, acc_xgb]
+        })
+
+        result_df = results.sort_values(by='Score', ascending=False)
+        result_df = result_df.set_index('Score')
+        print('Simple performance comparison:')
+        print(result_df.head())
+
+        #                      Model
+        # Score
+        # 93.38        Random Forest
+        # 92.70           XGBoosting
+        # 82.83  Logistic Regression
+        # 82.72           Linear SVM
+        # In simple comparison,  random forest has the best score
+
+
+        # Cross validation
+        # Logistic regression
+        logreg = LogisticRegression()
+        # Linear SVM
+        linear_svc = LinearSVC()
+        # Random forest
+        random_forest = RandomForestClassifier(n_estimators=100, oob_score=True)
+        # XGBoost
+        xgb = XGBClassifier(n_estimators=100)
+        models = [logreg, linear_svc, random_forest, xgb]
+        model_name = ['Logistic Regression', 'Linear SVM', 'Random Forest', 'XGBoosting']
+        accuracy_scores = []
+        accuracy_stds = []
+        f1_scores = []
+        f1_stds = []
+        roc_auc_scores = []
+        roc_auc_stds = []
+        for m in models:
+            scores = cross_validate(m, X_train, y_train, cv=10, scoring=['accuracy', 'f1', 'roc_auc'])
+            accuracy_scores.append(scores['test_accuracy'].mean())
+            accuracy_stds.append(scores['test_accuracy'].std())
+            f1_scores.append(scores['test_f1'].mean())
+            f1_stds.append(scores['test_f1'].std())
+            roc_auc_scores.append(scores['test_roc_auc'].mean())
+            roc_auc_stds.append(scores['test_roc_auc'].std())
+
+        results = {
+            'Model': model_name,
+            'Accuracy Mean Score': accuracy_scores,
+            'Accuracy Std':  accuracy_stds,
+            'F1 Mean Score': f1_scores,
+            'F1 Std': f1_stds,
+            'ROC_AUC Mean Score': roc_auc_scores,
+            'ROC_AUC Std': roc_auc_stds
+        }
+        model_comparison = pd.DataFrame(results)
+        print('Cross validation model performance comparison:')
+        print(model_comparison)
+
+        #                  Model  Accuracy Mean Score  Accuracy Std  F1 Mean Score    F1 Std  ROC_AUC Mean Score  ROC_AUC Std
+        # 0  Logistic Regression             0.828277      0.034122       0.763334  0.061616            0.871147     0.036434
+        # 1           Linear SVM             0.826030      0.027681       0.757139  0.053775            0.871224     0.036828
+        # 2        Random Forest             0.800262      0.045524       0.726704  0.077128            0.847436     0.070577
+        # 3           XGBoosting             0.812659      0.059696       0.744886  0.090067            0.857650     0.067001
+
+        # In cross validation, all 4 models performs quite similar
+
+        # Hyperparameter-tuning, very time-consuming, comment out
+        #param_grid = {"criterion": ["gini", "entropy"], "min_samples_leaf": [1, 5, 10, ],
+        #              "min_samples_split": [2, 4, 10, ], "n_estimators": [100, 500, 11000, 1500]}
+
+        #gd = GridSearchCV(estimator=RandomForestClassifier(random_state=42), param_grid=param_grid, verbose=True)
+        #gd.fit(X_train, y_train)
+        #print(gd.best_score_)
+        #print(gd.best_estimator_)
+
+        # Train Random forest to  with hyperparameters found in tuning
+        print()
+        print('train random forest with chosen hyperparameters')
+        random_forest = RandomForestClassifier(criterion='gini',
+                                               n_estimators=11000,
+                                               max_depth=7,
+                                               min_samples_split=6,
+                                               min_samples_leaf=5,
+                                               max_features='auto',
+                                               oob_score=True,
+                                               random_state=42,
+                                               n_jobs=-1,
+                                               verbose=1)
+        random_forest.fit(X_train, y_train)
+
+        print(f"random forest score {random_forest.score(X_train, y_train)}")
+        print(f"oob score: {round(random_forest.oob_score_, 4) * 100}")
+
+        # Save the model
+        self.model = random_forest
+
+    def predict(self):
+        """
+        make prediction with trained and saved model
+        :return:
+        """
+        X_test = StandardScaler().fit_transform(self.df_test)
+        return self.model.predict(X_test)
+
+    @staticmethod
+    def save_to_submission(y_prediction):
+        """
+        save prediction to csv file for Kaggle submission
+        Format:
+            PassengerId, Survived
+        :return: 
+        """
+        length = y_prediction.shape[0]
+        print(f'predictions rows: { length }')
+        if length != 418:
+            print('prediction length does not match expectation')
+            return
+
+        df = pd.DataFrame({
+            "PassengerId": [i for i in range(892, 1310)],
+            "Survived": y_prediction.astype('int32')
+        })
+
+        print(df.dtypes)
+        df.to_csv('submission.csv', index=False)
+
 
 def main():
     c = TitanicSurvival()
     c.load_data()
     c.explore_data()
     c.data_preprocessing()
+    c.train_and_evaluate_model()
+    y_prediction = c.predict()
+    c.save_to_submission(y_prediction)
 
 
 if __name__ == '__main__':
